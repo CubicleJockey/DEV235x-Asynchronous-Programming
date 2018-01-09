594d9880ee0a8042e90eb43c2275eff155bdcd28
André Davis <CubicleJockey@users.noreply.github.com>
2018-01-08 17:22:10 -0800

    Basic Setup

---
+++ AsyncProgrammingDev235x/AsyncProgrammingDev235x/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncProgrammingDev235x
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
