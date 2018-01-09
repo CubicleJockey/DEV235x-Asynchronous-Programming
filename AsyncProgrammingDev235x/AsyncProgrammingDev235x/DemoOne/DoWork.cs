3d710715d85415673ebad89196aad221343dfb83
André Davis <CubicleJockey@users.noreply.github.com>
2018-01-08 20:21:14 -0800

    Xml Sample

--- AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWork.cs
+++ AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWork.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using static System.Console;
+
+using System;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace AsyncProgrammingDev235x.DemoOne
 {
@@ -26,6 +30,36 @@ namespace AsyncProgrammingDev235x.DemoOne
                 result = client.DownloadStringTaskAsync(uri);
             }
             return result;
+        }
+
+        /// <summary>
+        /// Example of xmlFile I/O async.
+        /// 
+        /// books.xml was created from https://msdn.microsoft.com/en-us/library/ms762271%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+        /// </summary>
+        /// <param name="xmlFile">Xml File</param>
+        /// <returns></returns>
+        public async Task<string> CountNumberOfXmlNodes(FileInfo xmlFile)
+        {
+            xmlFile.Refresh();
+            if(!xmlFile.Exists) { throw new FileNotFoundException($"[{xmlFile.Name}] does not exist."); }
+
+            var count = 0;
+
+            var settings = new XmlReaderSettings { Async = true };
+
+            using (var fileStream = xmlFile.OpenRead())
+            {
+                using (var xmlReader = XmlReader.Create(fileStream, settings))
+                {
+                    while (await xmlReader.ReadAsync())
+                    {
+                        WriteLine(xmlReader.NodeType == XmlNodeType.Element ? xmlReader.Name : xmlReader.Value);
+                        count++;
+                    }       
+                }
+            }
+            return $"{count} total nodes!";
         }
     }
 }
--- AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWorkPlayGround.cs
+++ AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWorkPlayGround.cs
@@ -1,5 +1,7 @@
 using static System.Console;
 
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,12 +11,18 @@ namespace AsyncProgrammingDev235x.DemoOne
     [TestClass]
     public class DoWorkPlayGround
     {
+        private readonly DoWork doWork;
+
+        public DoWorkPlayGround()
+        {
+            doWork = new DoWork();
+        }
+
         [TestMethod]
-        public async Task HowManyTimesWebService_Basic()
+        public async Task HowManyTimesWebService()
         {
             const int count = 10;
-            var doWork = new DoWork();
-
+            
             var response = await doWork.HowManyTimesWebServiceAsync(count);
             
             response.Should().NotBeNullOrWhiteSpace();
@@ -23,9 +31,27 @@ namespace AsyncProgrammingDev235x.DemoOne
             WriteLine(response);
 
             response.Should()
-                    .BeEquivalentTo($"You've got {count} AsyncCoin!");
+                    .ContainEquivalentOf($"You've got {count} AsyncCoin!");
+        }
 
+        [TestMethod]
+        public async Task XmlFileIoExample()
+        {
+            var path =
+                Path.Combine(Environment.CurrentDirectory.Replace(@"\bin\Debug\netcoreapp2.0", string.Empty),
+                             @"Files\books.xml");
 
+            var file = new FileInfo(path);
+
+            WriteLine($"File: [{file}].");
+
+            var response = await doWork.CountNumberOfXmlNodes(file);
+
+            response.Should().NotBeNullOrWhiteSpace();
+
+            WriteLine(response);
+
+            response.Should().BeEquivalentTo("342 total nodes!");
         }
     }
 }
