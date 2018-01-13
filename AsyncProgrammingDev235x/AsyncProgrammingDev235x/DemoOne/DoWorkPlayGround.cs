79c78406605e342408a2772738ed0ae4898b48b0
André Davis <CubicleJockey@users.noreply.github.com>
2018-01-13 10:11:38 -0800

    Few examples added before resuming class.

--- AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWorkPlayGround.cs
+++ AsyncProgrammingDev235x/AsyncProgrammingDev235x/DemoOne/DoWorkPlayGround.cs
@@ -35,6 +35,28 @@ namespace AsyncProgrammingDev235x.DemoOne
         }
 
         [TestMethod]
+        public async Task HowManyTimesWebService_DoWorkWhileRunning()
+        {
+            const int count = 20;
+
+            //NOTE: Capturing the task triggers the execution
+            var task = doWork.HowManyTimesWebServiceAsync(count);
+            
+            WriteLine("Counting while web task runs.");
+            for (var i = 0; i < 10; i++) { WriteLine(i); }
+
+            var response = await task;
+
+            response.Should().NotBeNullOrWhiteSpace();
+
+            //Display in Test Runner Console
+            WriteLine(response);
+
+            response.Should()
+                .ContainEquivalentOf($"You've got {count} AsyncCoin!");
+        }
+
+        [TestMethod]
         public async Task XmlFileIoExample()
         {
             var path =
@@ -46,6 +68,32 @@ namespace AsyncProgrammingDev235x.DemoOne
             WriteLine($"File: [{file}].");
 
             var response = await doWork.CountNumberOfXmlNodes(file);
+
+            response.Should().NotBeNullOrWhiteSpace();
+
+            WriteLine(response);
+
+            response.Should().BeEquivalentTo("342 total nodes!");
+        }
+
+        [TestMethod]
+        public async Task XmlFileIoExample_DoWorkWhileRunning()
+        {
+            var path =
+                Path.Combine(Environment.CurrentDirectory.Replace(@"\bin\Debug\netcoreapp2.0", string.Empty),
+                    @"Files\books.xml");
+
+            var file = new FileInfo(path);
+
+            WriteLine($"File: [{file}].");
+
+            //NOTE: Capturing the task triggers the execution
+            var task = doWork.CountNumberOfXmlNodes(file);
+
+            WriteLine("Do some work while Xml File is being read.");
+            for(var i = 0; i < 15; i++) {  WriteLine(i); }
+
+            var response = await task;
 
             response.Should().NotBeNullOrWhiteSpace();
 
