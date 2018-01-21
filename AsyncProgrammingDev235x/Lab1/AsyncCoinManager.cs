c08b951738a23f268ec152e16ecf2a118ba95b1c
André Davis <CubicleJockey@users.noreply.github.com>
2018-01-21 09:12:29 -0800

    Starting Labs

---
+++ AsyncProgrammingDev235x/Lab1/AsyncCoinManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace Lab1
+{
+    public class AsyncCoinManager
+    {
+
+        public void AcquireAsyncCoin()
+        {
+            WriteLine($"Start call to long-running service at {DateTime.Now}");
+            var result = PretendToConnectToCoinService();
+            WriteLine($"Finish call to long-running service at {DateTime.Now}");
+
+            DisplayResultAsRed(result);
+        }
+
+        private static void DisplayResultAsRed(string result)
+        {
+            var savedColor = ForegroundColor;
+            ForegroundColor = ConsoleColor.Red;
+
+            WriteLine($"Result: [{result}]");
+
+            ForegroundColor = savedColor;
+        }
+
+        private static string PretendToConnectToCoinService()
+        {
+            Thread.Sleep(5000);
+            return "You've gotten 25 Async Coins";
+        }
+    }
+}
---
+++ AsyncProgrammingDev235x/Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace Lab1
+{
+    public class Program
+    {
+        private static readonly AsyncCoinManager asyncCoinManager;
+
+        static Program()
+        {
+            asyncCoinManager = new  AsyncCoinManager();
+        }
+
+        public static void Main(string[] args)
+        {
+            asyncCoinManager.AcquireAsyncCoin();
+
+            Write("Press any key to exit...");
+            ReadLine();
+        }
+    }
+}
